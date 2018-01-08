575a46bed27ebc558fcbb136f6b2ebec88c96260
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2018-01-08 08:22:27 +0100

    correction last norme and bug

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomlulu <tomlulu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/07 16:38:31 by tomlulu           #+#    #+#             */
-/*   Updated: 2018/01/07 22:59:25 by tomlulu          ###   ########.fr       */
+/*   Created: 2018/01/08 08:14:11 by tmaraval          #+#    #+#             */
+/*   Updated: 2018/01/08 08:19:05 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,19 @@ int		ft_cnt_digit(int nb, int base)
 	}
 	return (cnt);
 }
-char	*ft_itoa_base(int nb, int base)
-{
-	int sign;
-	char *ret;
-	char basestr[17];
-	int cnt;
-	int i;
 
-	sign = (nb < 0);
+char	*ft_itoa_cpy(int nb, int base, int sign)
+{
+	int		i;
+	char	*ret;
+	char	basestr[17];
+
 	i = 1;
-	if (sign == 1 && base == 10)
-		nb = nb * -1;
-	if (nb == 0)
-	{
-		ret = ft_memalloc(2);
-		ret[0] = '0';
-		return (ret);
-	}
 	cnt = ft_cnt_digit(nb, base);
 	ft_strcpy(basestr, "0123456789ABCDEF");
 	if (!(ret = (char *)malloc(sizeof(char) * (cnt + sign + 1))))
 		return (NULL);
-	while(nb != 0)
+	while (nb != 0)
 	{
 		ret[cnt + sign - i] = basestr[nb % base];
 		nb = nb / base;
@@ -55,5 +45,24 @@ char	*ft_itoa_base(int nb, int base)
 	if (sign == 1 && base == 10)
 		ret[0] = '-';
 	ret[cnt + sign] = '\0';
+	return (ret);
+}
+
+char	*ft_itoa_base(int nb, int base)
+{
+	int		sign;
+	int		cnt;
+	char	*ret;
+
+	sign = (nb < 0);
+	if (sign == 1 && base == 10)
+		nb = nb * -1;
+	if (nb == 0)
+	{
+		ret = ft_memalloc(2);
+		ret[0] = '0';
+		return (ret);
+	}
+	ret = ft_itoa_cpy(nb, base, sign);
 	return (ret);
 }
--- ft_strrev.c
+++ ft_strrev.c
@@ -6,26 +6,26 @@
 /*   By: tomlulu <tomlulu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/07 14:08:55 by tomlulu           #+#    #+#             */
-/*   Updated: 2018/01/07 21:09:17 by tomlulu          ###   ########.fr       */
+/*   Updated: 2018/01/08 08:21:09 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strrev(char *str)
+char	*ft_strrev(char *str)
 {
-    int i;
+	int i;
 	int j;
 
 	i = ft_strlen(str) - 1;
 	j = 0;
-    while (i > j)
-    {
-        str[i] = str[i] ^ str[j];
-        str[j] = str[i] ^ str[j];
+	while (i > j)
+	{
 		str[i] = str[i] ^ str[j];
-        i--;
-        j++;
-    }
-    return (str);
+		str[j] = str[i] ^ str[j];
+		str[i] = str[i] ^ str[j];
+		i--;
+		j++;
+	}
+	return (str);
 }
