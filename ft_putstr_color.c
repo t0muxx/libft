70a7abb9f378cf76153f0ad5be6f1b8ba65c9b93
tmaraval <tomlulu@Archlinux.localdomain>
2018-02-13 20:05:09 +0000

    modif pustr_color

--- ft_putstr_color.c
+++ ft_putstr_color.c
@@ -6,7 +6,7 @@
 /*   By: tmaraval <tmaraval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/05 10:46:02 by tmaraval          #+#    #+#             */
-/*   Updated: 2018/01/05 10:46:18 by tmaraval         ###   ########.fr       */
+/*   Updated: 2018/02/13 20:04:28 by tomlulu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 void	ft_putstr_color(char *str, char *color)
 {
+	color = !ft_strcmp("RED", color) ? ANSI_COLOR_RED : color;
+	color = !ft_strcmp("GREEN", color) ? ANSI_COLOR_GREEN : color;
+	color = !ft_strcmp("YELLOW", color) ? ANSI_COLOR_YELLOW : color;
+	color = !ft_strcmp("BLUE", color) ? ANSI_COLOR_BLUE : color;
+	color = !ft_strcmp("MAGENTA", color) ? ANSI_COLOR_MAGENTA : color;
+	color = !ft_strcmp("PURPLE", color) ? ANSI_COLOR_PURPLE : color;
+	color = !ft_strcmp("CYAN", color) ? ANSI_COLOR_CYAN : color;
 	ft_putstr(color);
 	ft_putstr(str);
 	ft_putstr(ANSI_COLOR_RESET);
