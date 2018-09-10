7ee0a35644750790b9768429309e5958231f8bc7
Tom MARAVAL <tmaraval@e1r6p20.42.fr>
2018-09-10 08:55:47 +0200

    added ft_lstfree

---
+++ ft_lstfree.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstfree.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmaraval <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/10 08:52:22 by tmaraval          #+#    #+#             */
+/*   Updated: 2018/09/10 08:54:32 by tmaraval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstfree(t_list *head)
+{
+	t_list *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->content);
+		free(temp);
+		temp = NULL;
+	}
+	head = NULL;
+}
